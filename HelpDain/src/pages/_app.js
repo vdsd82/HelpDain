77684cbd039ea9e32dbf1dc4f670bd34587505a6
vdsd82 <vijaydhanda@gmail.com>
2022-12-13 02:37:47 +0530

    commit

--- HelpDain/src/pages/_app.js
+++ HelpDain/src/pages/_app.js
@@ -5,24 +5,14 @@ import './index.scss';
 import { store } from '../redux/store';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { initializeApp } from "firebase/app";
+
 import SEO from '../components/seo';
 if (typeof window !== "undefined") {
   require("bootstrap/dist/js/bootstrap");
 }
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDbmhdQyYo42BS4i3zLAcUhZEk0FfPWgfA",
-  authDomain: "helpdain-903c9.firebaseapp.com",
-  projectId: "helpdain-903c9",
-  storageBucket: "helpdain-903c9.appspot.com",
-  messagingSenderId: "365643903871",
-  appId: "1:365643903871:web:ffa41433f1a55519e5a04a",
-measurementId: "G-437F9NCKFE"
 
-};
-initializeApp(firebaseConfig);
 
 
 function MyApp({ Component, pageProps }) {
