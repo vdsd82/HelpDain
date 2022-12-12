d1c406632ef5ff165792fc5d831b15989d423cf9
vdsd82 <vijaydhanda@gmail.com>
2022-12-13 00:11:03 +0530

    commit

--- HelpDain/src/pages/about/index.js
+++ HelpDain/src/pages/about/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import AboutArea from '../../components/About/AboutArea';
 import BgShape from '../../components/common/BgShape';
-import BrandArea from '../../components/Home/BrandArea';
 import Footer from '../../components/Home/Footer';
 import Header from '../../components/Home/Header';
 import SEO from '../../components/seo';
@@ -13,7 +12,6 @@ const About = () => {
          <Header />
          <BgShape />
          <AboutArea />
-         <BrandArea spacing="pb-65 pt-80" />
          <Footer />
       </>
    );
